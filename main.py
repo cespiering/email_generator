e2ef1247ee8d4e989d3db89522e8a4cfc253da15
cespiering <59517544+cespiering@users.noreply.github.com>
2021-12-07 11:36:08 -0800

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# create a app password for the gmail
+import email_module
+
+class Template:
+    def __init__(self, company_name, company_email):
+        self.company_name = company_name
+        self.company_email = company_email
+
+    def populate_template(self):
+        return template.txt formatted
+
+    def send_email(self):
+        return email_module.send_email(self.company_email, self.company_name)
+
+pinwheel = Template('Pinwheel','pinwheel@pinwheel.com')
+
+pinwheel.send_email()
+
+#Template:
+class User:
+    your_email
+    your project_name
+    your phone_number
+
+carly = User("cespiering")
+shane = User()
+
+shane.send_email
+
